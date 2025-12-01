2fc256217b2c57513dcfaac5e6c4db5cc7426c50
EmreeCannn <emrecangozleyici@gmail.com>
2025-12-01 22:37:40 +0300

    fix: remove edge runtime

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import app from "../src/index.js";
 import serverless from "serverless-http";
 
 export const config = {
-  runtime: "nodejs18.x",
+  runtime: "nodejs18.x"
 };
 
 export default serverless(app);
